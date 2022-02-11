cdcdde2fc4b479ce25998a91ef3b48448c2dbdda
komediantto <komediantto@yandex.ru>
2022-02-11 19:34:28 +0200

    Fourth commit

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'default': {
             'ENGINE': os.environ.get('DB_ENGINE',
                                      default='django.db.backends.postgresql'),
-            'NAME': os.environ.get('DB_NAME'),
+            'NAME': os.environ.get('POSTGRES_DB'),
             'USER': os.environ.get('POSTGRES_USER'),
             'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
             'HOST': os.environ.get('DB_HOST'),
